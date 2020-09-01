2c04ace1b9c592874aba12f56a86c2f9c8ce1b33
RAJALAKSHMIJ1013 <70467891+RAJALAKSHMIJ1013@users.noreply.github.com>
2020-09-01 16:58:40 +0530

    LETSUPGRADE JAVASCRIPT ESSENTIALS Day 2-assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+// 1.to search a particular character in a  string
+let str="letsupgrade";
+let res=str.charAt(2);
+console.log(res);
+// 2.conversion of minutes to seconds
+let minutes=5
+let seconds=minutes*60;
+console.log("seconds="+seconds);
+// 3.search an element in an array
+let array=["cake","rosemilk","choco","icecream","sweets"]
+let A=array.indexOf("choco");
+console.log(A);
+// 4.from the array display only elements containing "a"
+ let a=["karthick","kayal","gilli","kanama","ussili"];
+ let happy=a.every(function(e){
+	 return e==a;
+ });
+console.log(happy);
+// 5.print array in reverse
+let v=["vishali","vanaja","varun","vishnu"];
+console.log("reverse of array="+v.reverse());
+
+
+
